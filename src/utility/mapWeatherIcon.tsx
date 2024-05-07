c85e141589e1991d2dd55d7e42f87d9b33a1759b
Harri Huhtala <bodhisatva@users.noreply.github.com>
2024-05-07 10:48:22 +0300

    feat(map-weather-icons): night time icons

--- src/utility/mapWeatherIcon.tsx
+++ src/utility/mapWeatherIcon.tsx
@@ -5,18 +5,30 @@ import Rain from '@/icons/rain.svg'
 import Thunderstorm from '@/icons/thunderstorm.svg'
 import Snowy from '@/icons/snowing.svg'
 import Mist from '@/icons/mist.svg'
+import Moon from '@/icons/clear-night.svg'
+import PartlyCloudyNight from '@/icons/partly-cloudy-night.svg'
+import Showers from '@/icons/rainy-light.svg'
 
 const mapWeatherIconCode = (iconCode: string) => {
   const iconMap: Record<string, any> = {
-    '01d': Sun,
-    '02d': PartlyCloudy,
-    '03d': Cloud,
-    '04d': Cloud,
-    '09d': Rain,
-    '10d': Rain,
-    '11d': Thunderstorm,
-    '13d': Snowy,
-    '50d': Mist
+    '01d': Sun, // clear sky
+    '01n': Moon, // clear sky at night
+    '02d': PartlyCloudy, // few clouds
+    '02n': PartlyCloudyNight, // few clouds at night
+    '03d': Cloud, // scattered clouds
+    '03n': Cloud, // scattered clouds at night
+    '04d': Cloud, // broken clouds
+    '04n': Cloud, // broken clouds at night
+    '09d': Showers, // shower rain
+    '09n': Showers, // shower rain at night
+    '10d': Rain, // rain
+    '10n': Rain, // rain at night
+    '11d': Thunderstorm, // thunderstorm
+    '11n': Thunderstorm, // thunderstorm at night
+    '13d': Snowy, // snow
+    '13n': Snowy, // snow at night
+    '50d': Mist, // mist
+    '50n': Mist // mist at night
   }
 
   return iconMap[iconCode]
@@ -27,15 +39,3 @@ export const createIcon = (iconCode: string) => {
 
   return <Icon />
 }
-
-// const descriptionMap = {
-//   '01d': 'sun',
-//   '02d': 'few clouds',
-//   '03d': 'scattered clouds',
-//   '04d': 'broken clouds',
-//   '09d': 'shower rain',
-//   '10d': 'rain',
-//   '11d': 'thunderstorm',
-//   '13d': 'snow',
-//   '50d': 'mist'
-// }
